dbf40cf2894cc6209066ff94cadf18ca0ba7cb37
Steve Solis <steve@Air-de-Steve-2.lan>
2026-02-27 18:45:42 -0500

    Añadir evento

--- ticketsystem/settings.py
+++ ticketsystem/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # ==============================
-# SEGURIDADholi
+# SEGURIDADholil
 # ==============================
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "clave-insegura-solo-desarrollo")
