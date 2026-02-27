3600b8c3d669f77c47d6c67de31fc13288c535eb
Steve Solis <steve@192.168.1.15>
2026-02-27 17:13:55 -0500

    Trigger deploy

--- ticketsystem/urls.py
+++ ticketsystem/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-#Holis
-urlpatterns = [ 
+
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('boletos.urls')),
 ]
