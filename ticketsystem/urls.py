2689e8611a31e32bfaeb32661475fd7159aedb69
Steve Solis <steve@Air-de-Steve-2.lan>
2026-02-26 20:36:53 -0500

    Agregar página principal

--- ticketsystem/urls.py
+++ ticketsystem/urls.py
@@ -1,5 +1,6 @@
+from django.contrib import admin
+from django.urls import path
 from django.http import HttpResponse
-from django.urls import path, include
 
 def home(request):
     return HttpResponse("Sistema de boletos funcionando correctamente")
