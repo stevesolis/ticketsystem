f6afe3f3bdc06de631a1f92fd91271d998146b52
Steve Solis <steve@192.168.1.15>
2026-02-28 16:32:13 -0500

    Agregada página de inicio con imagen pública

--- boletos/urls.py
+++ boletos/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     # Usamos <uuid:codigo> porque tu código tiene formato de UUID
     # Si falla, podemos usar <path:codigo> que acepta cualquier caracter
+    path('', views.inicio, name='inicio'), # Esta es la página de inicio pública
     path('validar/<path:codigo>/', views.validar_boleto, name='validar_boleto'),
     path('dashboard/', views.estadisticas, name='dashboard'),
 ]
